3ecd2e4d862e03c579147b0d895b0b1c57f35e2e
Kevin Jaget <kjaget@nc.rr.com>
2022-12-16 23:27:55 -0500

    More work on day16

--- day16/src/p1.cpp
+++ day16/src/p1.cpp
@@ -3,6 +3,7 @@
 #include <deque>
 #include <fstream>
 #include <functional>
+#include <ios>
 #include <iostream>
 #include <iterator>
 #include <map>
@@ -14,6 +15,7 @@
 #include <set>
 #include <vector>
 
+using Pressure = uint64_t;
 class Node
 {
 public:
@@ -41,6 +43,7 @@ public:
     Node(const Node &other) = default;
     const std::string &get_name(void) const { return name_; }
     uint8_t get_flow_rate(void) const { return flow_rate_; }
+    const std::vector<std::string> &get_edge_names(void) const { return edge_names_; }
     std::optional<uint32_t> get_valve_index(void) const
     {
         if (flow_rate_ == 0)
@@ -48,6 +51,16 @@ public:
             return std::nullopt;
         }
         return valve_index_;
+    }
+    bool is_valve_opened(uint32_t valve_mask) const
+    {
+        if (valve_index_ == std::numeric_limits<uint8_t>::max())
+        {
+            std::cout << "is_valve_opened called on Node with flow == 0"<< std::endl;
+            return false;
+        }
+        uint32_t valve_bit = 1UL << valve_index_;
+        return (valve_mask & valve_bit) == valve_bit;
     }
     friend std::ostream &operator<<(std::ostream &os, const Node &n)
     {
@@ -62,7 +75,6 @@ public:
 private:
     uint8_t valve_index_{std::numeric_limits<uint8_t>::max()};
     uint8_t flow_rate_{0};
-    //std::vector<const Node &> edges_;
     std::vector<std::string> edge_names_;
     std::string name_;
 };
@@ -72,53 +84,111 @@ class Move
 {
 public:
     Move(const NodeMap &node_map)
-    : current_node_{node_map.at("AA")}, prev_node_{node_map.at("AA")}
+    : current_node_{&node_map.at("AA")}
     {
         minute_ = 0;
+        prev_node_name_ = "AA";
     }
-    Move(const NodeMap &node_map, const Move &prev_move, const std::string &next_node_str)
-        : current_node_{node_map.at(next_node_str)}, prev_node_{node_map.at(prev_move.current_node_.get_name())}
+    Move(const NodeMap &node_map, const uint8_t max_minutes, const Move &prev_move, const std::string &next_node_str)
+        : current_node_{&node_map.at(next_node_str)}
     {
-        minute_ += 1;
+        const Node &prev_node = node_map.at(prev_move.current_node_->get_name());
+        pressure_ = prev_move.pressure_;
+        valves_opened_ = prev_move.valves_opened_;
+        minute_ = prev_move.minute_ + 1;
         // staying in same location, opening valve
-        if (current_node_.get_name() == prev_node_.get_name())
+        if (current_node_->get_name() == prev_node.get_name())
         {
-            if (prev_node_.get_name() != "AA")
-            {
-                valves_opened_ = prev_move.valves_opened_ | 1UL << *(current_node_.get_valve_index());
-                if (prev_move.pressure_ == 0)
-                {
-                    pressure_ = current_node_.get_flow_rate();
-                }
-                else
-                {
-                    pressure_ = prev_move.pressure_ * current_node_.get_flow_rate();
-                }
-            }
+            valves_opened_ |= 1UL << *(current_node_->get_valve_index());
+            pressure_ += current_node_->get_flow_rate() * (max_minutes - prev_move.minute_);
+            prev_node_was_zero_ = prev_move.prev_node_was_zero_;
+            prev_node_name_ = prev_move.prev_node_name_;
         }
-        if (prev_move.current_node_.get_flow_rate() == 0 ||
-            ((*(prev_move.current_node_.get_valve_index())) & prev_move.valves_opened_))
+        else if (prev_move.current_node_->get_flow_rate() == 0 ||
+                prev_move.current_node_->is_valve_opened(valves_opened_))
         {
             prev_node_was_zero_ = true;
-            prev_node_name_ = prev_node_.get_name();
+            prev_node_name_ = prev_node.get_name();
+        }
+        else
+        {
+            prev_node_name_ = prev_node.get_name();
         }
     }
+    std::vector<Move> next_moves(const NodeMap &node_map, const uint8_t max_minutes) const
+    {
+        std::vector<Move> ret;
 
+        const auto &edge_names = current_node_->get_edge_names();
+        for (const auto &e : edge_names)
+        {
+            // Avoid loopoing over nodes with 0 flow rate
+            if (prev_node_was_zero_ && (prev_node_name_ == e))
+            {
+                continue;
+            }
+            ret.emplace_back(Move(node_map, max_minutes, *this, e));
+        }
+
+        // If there's a valve to open that hasn't been, one new move is
+        // to open it
+        const auto valve_index = current_node_->get_valve_index();
+        if (valve_index)
+        {
+            const uint32_t valve_mask = 1UL << *valve_index;
+            if ((valve_mask & valves_opened_) == 0)
+            {
+                ret.emplace_back(Move(node_map, max_minutes, *this, current_node_->get_name()));
+            }
+        }
+
+        return ret;
+    }
+    std::optional<Pressure> final_score(size_t minutes) const
+    {
+        if (minute_ == minutes)
+        {
+            return pressure_;
+        }
+        return std::nullopt;
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const Move &m)
+    {
+        os << "minute = " << static_cast<int>(m.minute_) << " prev_node_was_zero = " << m.prev_node_was_zero_ << " prev_node_name = " << m.prev_node_name_ << " pressure = " << m.pressure_<< " valves_opened = " << std::hex << m.valves_opened_ << std::dec << " score = " << m.score() << " current_node = " << *m.current_node_;
+        return os;
+    }
+    long score(void) const
+    {
+        long score = pressure_;
+        score -= static_cast<long>(minute_) * 5;
+        auto flow_rate = current_node_->get_flow_rate();
+        if (flow_rate && !current_node_->is_valve_opened(valves_opened_))
+        {
+            score += (30 - (minute_ - 1)) * flow_rate;
+        }
+
+        return score;
+    }
+
+    bool operator<(const Move &other) const
+    {
+        return score() < other.score();
+    }
 private:
     uint8_t minute_;
     bool prev_node_was_zero_{false};
     uint16_t valves_opened_{0};
-    uint32_t pressure_{0};
-    const Node &prev_node_;
-    const Node &current_node_;
+    Pressure pressure_{};
+    const Node *current_node_;
     std::string prev_node_name_;
 };
-
 
 int main(int argc, char **argv)
 {
     std::ifstream istream(argv[1], std::ifstream::in);
     std::string line;
+    constexpr size_t minutes = 30;
 
     NodeMap nodes;
     uint8_t valve_idx = 0; // static class member?
@@ -128,5 +198,34 @@ int main(int argc, char **argv)
         nodes[n.get_name()] = n;
         std::cout << n << std::endl;
     }
+
+    std::priority_queue<Move> moves;
+    moves.push(Move(nodes));
+
+    Pressure best_pressure = 0;
+    while(!moves.empty())
+    {
+        const auto move = moves.top();
+        moves.pop();
+        //std::cout << "Move = " << move << std::endl;
+        const auto this_pressure = move.final_score(minutes);
+        if (this_pressure)
+        {
+            if (*this_pressure > best_pressure)
+            {
+                best_pressure = std::max(best_pressure, *this_pressure);
+                std::cout << "New best pressure = " << best_pressure << std::endl;
+            }
+            continue;
+        }
+
+        const auto next_moves = move.next_moves(nodes, minutes);
+        for (const auto &nm : next_moves)
+        {
+            //std::cout << "    Next move = " << nm << std::endl;
+            moves.push(nm);
+        }
+    }
+
     return 0;
 }
